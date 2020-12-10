2da863bbcf469aea2623a58145d4c1297be53364
Jay <jvelayud@gmail.com>
2020-12-10 17:23:02 -0500

    Adding a script to generate tests for different serverless versions

---
+++ tests/helpers/setup-versions.js
@@ -0,0 +1,56 @@
+const path = require("path");
+const fs = require("fs-extra");
+
+const versions = ["1.55.0", "1.77.1"];
+
+const testsDir = path.join(process.cwd(), "tests");
+const templateTestPath = path.join(testsDir, "serverless-versions");
+
+// Remove node_modules from tempalte test
+fs.removeSync(path.join(templateTestPath, "node_modules"));
+
+versions.forEach(copyVersion);
+
+function copyVersion(version) {
+  const newPath = path.join(
+    testsDir,
+    processString("serverless-versions-%version-string%", version)
+  );
+
+  fs.copySync(templateTestPath, newPath);
+
+  // Remove old lockfile
+  fs.removeSync(path.join(newPath, "package-lock.json"));
+
+  // Setup files
+  processFile(path.join(newPath, "package.json"), version);
+  processFile(path.join(newPath, "serverless.yml"), version);
+  processFile(
+    path.join(newPath, "serverless-versions.test.template"),
+    version,
+    path.join(
+      newPath,
+      processString("serverless-%version-string%.test.js", version)
+    )
+  );
+}
+
+function processString(str, version) {
+  const versionString = version.replace(/\./g, "");
+
+  return str
+    .replace(/%version%/g, version)
+    .replace(/%version-string%/g, versionString);
+}
+
+function processFile(templatePath, version, toFile) {
+  toFile = toFile || templatePath;
+
+  const template = fs.readFileSync(templatePath, { encoding: "utf-8" });
+  fs.writeFileSync(toFile, processString(template, version));
+
+  // If moving the file, remove the template
+  if (toFile !== templatePath) {
+    fs.removeSync(templatePath);
+  }
+}
--- tests/serverless-1771/handler.js
+++
@@ -1,6 +0,0 @@
-module.exports.hello = () => {
-  return {
-    statusCode: 200,
-    body: "Go Serverless v2.0! Your function executed successfully!",
-  };
-};
